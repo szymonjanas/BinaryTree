6cd3fe4601ee9796fb925dc5ebaca73923217362
[szymon] <[szymon.janas.95@gmail.com]>
2018-10-07 20:05:11 +0200

    added Tree class

--- inc/Tree.hpp
+++ inc/Tree.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "Node.hpp"
+
+template<tyoename T>
+class Tree
+{
+    shared_ptr<Node<T>> head;
+
+    unsigned int treeSize;
+
+    public:
+
+    shared_ptr<Node<T>> getHead() const noexcept;
+    unsigned int getTreeSize() const noexcept;
+
+    void push();
+    
+};
